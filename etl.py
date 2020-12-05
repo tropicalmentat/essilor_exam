45c35e83c72ceed353cbbc7d2f67dc32fbcd8150
tropicalmentat <gabtorres1011@gmail.com>
2020-12-05 21:35:05 +0800

    Ingestion and transformation workflow accomplished. Need to refine code for cleaner input and output.

--- etl.py
+++ etl.py
@@ -10,34 +10,77 @@
 import sqlite3
 import csv
 
-class etl(content):
-	def __init__(self):
+class etl():
+
+	def __init__(self,content):
+
 		self.data = content.readlines()
-		self.header = transform_header() 
-		self.ddl = ""
-		self.database = sqlite3.connect(':memory:')
+		self.header = self.transform_header()
+		self.ddl = self.build_ddl()
+
+		self.connection = self.create_db()
+		self.cursor = self.connection.cursor()
+
 		self.child_fns = list()
 
+	def create_db(self):
+		return sqlite3.connect(':memory:')
+
 	def transform_header(self):
-		self.header = self.data[0].upper().replace('\n','')
+		return self.data[0].upper().replace('\n','')
 
 	def build_ddl(self):
 		"""
-		Build ddl from headers of source file
+		Build data definition language from headers of source file.
+		All fields defined as sqlite3 TEXT
 		"""
 
-		q_string = [elm + ' text' for elm in headers.split(',')]
+		q_string = [elm + ' text' for elm in self.header.split(',')]
 
 		fields = '(' + ', '.join(q_string) + ')'
 
-		self.ddl = '''CREATE TABLE tmp ''' + fields
+		return '''CREATE TABLE tmp ''' + fields
 		
 	
 	def build_tmp(self):
-		pass
+		"""
+		Create tmp sqlite table in memory.
+		"""
+		insert_stmt = "INSERT INTO tmp VALUES "
+
+		def enclose(values):
+
+			return ','.join(["'" + v + "'" for v in values.split(',')])
+
+		def concatenate(values):
+
+			return insert_stmt + '(' + enclose(values.replace('\n', '')) + ')' 
+
+		cmds = list(map(concatenate, self.data))
+
+		self.cursor.execute(self.ddl)
+
+		for i in cmds:
+			self.cursor.execute(i)
+
+		return
 
 	def split(self):
-		pass
+		self.cursor.execute('SELECT DISTINCT SUB_ENTITY FROM tmp')
+	
+		uniq = [v[0] for v in self.cursor.fetchall()]
+
+		for v in uniq:
+
+			self.cursor.execute('SELECT * FROM tmp WHERE SUB_ENTITY="{}"'.format(v))
+
+			with open('{}.csv'.format(v),"w") as output:
+				csv_out = csv.writer(output)
+
+				csv_out.writerow(self.header.split(','))
+
+				for result in self.cursor:
+					csv_out.writerow(result)
 
 
 def build_ddl(headers):
@@ -56,7 +99,7 @@ def build_tmp(cursor, ddl, data):
 	"""
 	Create tmp sqlite table in memory.
 	"""
-	insert = "INSERT INTO tmp VALUES "
+	insert_stmt = "insert_stmt INTO tmp VALUES "
 
 	def enclose(values):
 
@@ -64,7 +107,7 @@ def build_tmp(cursor, ddl, data):
 
 	def concatenate(values):
 
-		return insert + '(' + enclose(values.replace('\n', '')) + ')' 
+		return insert_stmt + '(' + enclose(values.replace('\n', '')) + ')' 
 
 	cmds = list(map(concatenate, data))
 
@@ -73,6 +116,7 @@ def build_tmp(cursor, ddl, data):
 
 	for i in cmds:
 		cursor.execute(i)
+
 
 	return
 
@@ -119,6 +163,21 @@ def _main():
 
 	conn.close()
 
+def main():
+	fn = r'data/example-file.csv'
+
+	data = open(fn,'r')
+
+	transformer = etl(data)
+
+	# print(transformer.ddl)
+
+	transformer.build_tmp()
+
+	transformer.split()
+
+	# for r in transformer.cursor.execute('SELECT * FROM tmp LIMIT 10'):
+	# 	print(r)
 
 if __name__ == '__main__':
 	main()
